3f556001b8e3b8b12de017d2e6c3624b062644d6
Oskar <o.piechowski@gmail.com>
2022-11-25 23:54:48 +0100

    commit homework2 by Oskar

--- src/test/java/pl/sdacademy/java/patterns/TeacherBuilderTest.java
+++ src/test/java/pl/sdacademy/java/patterns/TeacherBuilderTest.java
@@ -8,11 +8,9 @@ public class TeacherBuilderTest {
 
     @Test
     void shouldCreateTeacherWithSubject(){
-        //when
         Teacher teacher = new TeacherBuilder()
                 .subjectCode(Subject.ENG)
                 .build();
-        //then
         assertThat(teacher).isNotNull();
         assertThat(teacher.getSubjectCodes()).isEqualTo(Subject.ENG);
 
